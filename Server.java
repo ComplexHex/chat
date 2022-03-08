959302b11942fd84e935d3ccb523be0ef51dda4d
ComplexHex <60725024+ComplexHex@users.noreply.github.com>
2022-03-08 18:31:09 +0300

    v8

--- Server.java
+++ Server.java
@@ -8,33 +8,22 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
 public class Server {
-
     private static Map<String, Connection> connectionMap = new ConcurrentHashMap<>();
-
 
     public static void main(String[] args) {
         ConsoleHelper.writeMessage("Введите порт сервера:");
         int port = ConsoleHelper.readInt();
 
         try (ServerSocket serverSocket = new ServerSocket(port)) {
-            ConsoleHelper.writeMessage("Чат сервера запущен");
+            ConsoleHelper.writeMessage("Чат сервер запущен.");
             while (true) {
+                // Ожидаем входящее соединение и запускаем отдельный поток при его принятии
                 Socket socket = serverSocket.accept();
                 new Handler(socket).start();
             }
-
         } catch (Exception e) {
-            ConsoleHelper.writeMessage("Произошла ошибка при запуске или работе сервара.");
+            ConsoleHelper.writeMessage("Произошла ошибка при запуске или работе сервера.");
         }
-    }
-
-    public static void sendBroadcastMessage(Message message) {
-        for (String string : connectionMap.keySet())
-            try {
-                connectionMap.get(string).send(message);
-            }catch (IOException e){
-                System.out.println("Сообщение не отправлено");
-            }
     }
 
     private static class Handler extends Thread {
@@ -43,12 +32,50 @@ public class Server {
         public Handler(Socket socket) {
             this.socket = socket;
         }
+
+        @Override
+        public void run() {
+
+        }
+
+        private String serverHandshake(Connection connection) throws IOException, ClassNotFoundException {
+            while (true) {
+                connection.send(new Message(MessageType.NAME_REQUEST));
+
+                Message message = connection.receive();
+                if (message.getType() != MessageType.USER_NAME) {
+                    ConsoleHelper.writeMessage("Получено сообщение от " + socket.getRemoteSocketAddress() + ". Тип сообщения не соответствует протоколу.");
+                    continue;
+                }
+
+                String userName = message.getData();
+
+                if (userName.isEmpty()) {
+                    ConsoleHelper.writeMessage("Попытка подключения к серверу с пустым именем от " + socket.getRemoteSocketAddress());
+                    continue;
+                }
+
+                if (connectionMap.containsKey(userName)) {
+                    ConsoleHelper.writeMessage("Попытка подключения к серверу с уже используемым именем от " + socket.getRemoteSocketAddress());
+                    continue;
+                }
+                connectionMap.put(userName, connection);
+
+                connection.send(new Message(MessageType.NAME_ACCEPTED));
+                return userName;
+            }
+        }
     }
 
-
-    public void run() {
-
+    public static void sendBroadcastMessage(Message message) {
+        // Рассылаем сообщение по всем соединениям
+        for (Connection connection : connectionMap.values()) {
+            try {
+                connection.send(message);
+            } catch (IOException e) {
+                ConsoleHelper.writeMessage("Не смогли отправить сообщение " + connection.getRemoteSocketAddress());
+            }
+        }
     }
-
 
 }
